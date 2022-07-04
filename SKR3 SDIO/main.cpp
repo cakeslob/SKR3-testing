8c63d266aa4efc62b782e9730c50b5d7fecfbd31
scott <scottalford75@gmail.com>
2022-07-04 13:33:31 +1000

    SKRv3 SDIO testing

---
+++ SKR3 SDIO/main.cpp
@@ -0,0 +1,66 @@
+#include "mbed.h"
+#include <cstdio>
+#include <cerrno>
+#include <string> 
+#include "FATFileSystem.h"
+
+#include "SDIOBlockDevice.h"
+
+SDIOBlockDevice blockDevice;
+FATFileSystem fileSystem("fs");
+FILE *jsonFile;
+string strJson;
+
+void readJsonConfig()
+{
+    printf("1. Reading json configuration file\n");
+
+    // Try to mount the filesystem
+    printf("Mounting the filesystem... ");
+    fflush(stdout);
+ 
+    int err = fileSystem.mount(&blockDevice);
+    printf("%s\n", (err ? "Fail :(" : "OK"));
+    if (err) {
+        printf("No filesystem found... ");
+        fflush(stdout);
+     }
+
+    // Open the config file
+    printf("Opening \"/fs/config.txt\"... ");
+    fflush(stdout);
+    jsonFile = fopen("/fs/config.txt", "r+");
+    printf("%s\n", (!jsonFile ? "Fail :(" : "OK"));
+
+    fseek (jsonFile, 0, SEEK_END);
+    int32_t length = ftell (jsonFile);
+    fseek (jsonFile, 0, SEEK_SET);
+
+    printf("Json config file lenght = %2d\n", length);
+
+    strJson.reserve(length+1);
+
+    while (!feof(jsonFile)) {
+        int c = fgetc(jsonFile);
+        strJson.push_back(c);
+    }
+
+    // Remove comments from next line to print out the JSON config file
+    //printf("%s\n", strJson.c_str());
+
+    printf("\rClosing \"/fs/config.txt\"... ");
+    fflush(stdout);
+    fclose(jsonFile);
+}
+
+
+// main() runs in its own thread in the OS
+int main()
+{
+    readJsonConfig();
+
+    while (true) {
+
+    }
+}
+
